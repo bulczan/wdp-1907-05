fdf98a6c95c820455432ddcff2db74dfdf64a12d
Jędrzej Krawczyk <siewierjedr@gmail.com>
2019-07-30 09:53:58 +0200

    resolving transition mustache to parcel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,5 @@
 console.log('hello');
+
+import './templatesGenerator.js';
+
+console.log('hello');
--- src/scripts/templatesGenerator.js
+++ src/scripts/templatesGenerator.js
@@ -1,29 +1,33 @@
-(function () {
-  function loadPost () {
-    var postTemplate = document.getElementById('post-box-column-template').innerHTML;
-    Mustache.parse(postTemplate); // optional, speeds up future uses
-    var rendered = '';
-    for (var i = 0; i < post.length; i++) {
-      rendered += Mustache.render(postTemplate, post[i]);
-    }
-    document
-      .querySelector('.section--blog-posts .row-posts')
-      .insertAdjacentHTML('beforeend', rendered);
-  }
+(function(){
+const Mustache = require('../vendor/Mustache.min.js');
+const product = require('./product_data.js');
+const post = require('./post_data.js');
 
-  function loadProducts () {
-    var productTemplate = document.getElementById('product-box-column-template')
-      .innerHTML;
-    Mustache.parse(productTemplate); // optional, speeds up future uses
-    var rendered = '';
-    for (var i = 0; i < product.length; i++) {
-      rendered += Mustache.render(productTemplate, product[i]);
-    }
-    document
-      .querySelector('.section--products .row-products')
-      .insertAdjacentHTML('beforeend', rendered);
-  }
+function loadPost () {
+var postTemplate = document.getElementById('post-box-column-template').innerHTML;
+Mustache.parse(postTemplate); // optional, speeds up future uses
+var rendered = '';
+for (var i = 0; i < post.length; i++) {
+    rendered += Mustache.render(postTemplate, post[i]);
+}
+document
+    .querySelector('.section--blog-posts .row-posts')
+    .insertAdjacentHTML('beforeend', rendered);
+}
 
-  loadPost();
-  loadProducts();
+function loadProducts () {
+var productTemplate = document.getElementById('product-box-column-template')
+    .innerHTML;
+Mustache.parse(productTemplate); // optional, speeds up future uses
+var rendered = '';
+for (var i = 0; i < product.length; i++) {
+    rendered += Mustache.render(productTemplate, product[i]);
+}
+document
+    .querySelector('.section--products .row-products')
+    .insertAdjacentHTML('beforeend', rendered);
+}
+
+loadPost();
+loadProducts();
 })();
