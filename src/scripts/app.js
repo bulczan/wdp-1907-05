fe737e5276dd40798305452a44f8ac8a9000f2eb
Darek.Szotowicz <dariusz.szotowicz@protonmail.com>
2019-08-03 14:30:19 +0200

    WDP190705-11, Add RWD to main menu

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,22 @@
 console.log('hello');
+
+{
+  // Header main menu open animation
+
+  const mobileMainMenuTrigger = document.querySelector('.mobile-menu-trigger-button');
+  const mobileMainMenu = document.querySelector('.menu-bar');
+  const mobileMainMenuActiveItem = document.querySelector(
+    '.menu > ul > li:first-child a'
+  );
+
+  const showMainMenu = () => {
+    mobileMainMenu.classList.toggle('open');
+    if (mobileMainMenu.classList.contains('open')) {
+      mobileMainMenuActiveItem.classList.add('active');
+    } else {
+      mobileMainMenuActiveItem.classList.remove('active');
+    }
+  };
+
+  mobileMainMenuTrigger.addEventListener('click', showMainMenu);
+}
