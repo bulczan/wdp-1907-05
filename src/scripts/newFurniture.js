5c817bc6a1493f3f257c894607b136605bfbe6f5
Jędrzej Krawczyk <siewierjedr@gmail.com>
2019-08-04 12:21:55 +0200

    Ended task WDP190705-17

--- src/scripts/newFurniture.js
+++ src/scripts/newFurniture.js
@@ -1,23 +1,57 @@
-var product = require('./product_data.js');
+const product = require('./product_data.js');
+const productGenerator = require('./templatesGenerator.js')
 
-var productToShow = [];
-var tabs = document.querySelectorAll('.section--products .col.menu ul a');
-var active = document.querySelector('.section--products .col.menu ul a.active');
-console.log(tabs);
-console.log(active);
+let tabs = document.querySelectorAll('.section--products .col.menu ul a');
+let active = document.querySelector('.section--products .col.menu ul a.active');
+const rowproducts = document.querySelector('.section--products .row-products');
 
-document.addEventListener('onclick', function(event){
-    tabs.forEach(function(elem){
-        elem.classList.remove('active');
-    });
-    event.target.classList.add('active');
+let productToShow = [];
 
-    
+document.addEventListener('DOMContentLoaded', () => { // Added Event for start content
+    rowproducts.innerHTML = "";
+    fadeAnimation();
+    productFiller();
+    productGenerator(productToShow);
 });
 
-for (var i = 0; i < product.length; i++) {
-    if (active.innerHTML.toLowerCase() === product[i].type){
-        productToShow =+ product[i];
-    }
+tabs.forEach((elem) => { // Added Event for every tab
+    elem.addEventListener('click', (event) => {
+        productToShow = [];
+        rowproducts.innerHTML = "";
+        fadeAnimation();
+        if(event.target !== active){ // Remove .active class for every tab
+            tabs.forEach( (elem) => {
+                elem.classList.remove('active');
+            });
+            event.target.classList.add('active'); // Add .active class for focus tab
+            active = document.querySelector('.section--products .col.menu ul a.active');
+
+            productFiller();
+            productGenerator(productToShow);
+        }
+    });
+});
+
+
+
+function fadeAnimation(){
+    let opa = 0.01;
+     setInterval(() =>{
+        if(opa >= 1){
+        clearInterval();
+        } else{
+        opa += opa * 0.1;
+        rowproducts.style.opacity = opa;
+        }
+    }, 10);
 };
-console.log(productToShow);
+
+function productFiller(){
+    for (var i = 0; i < product.length; i++) {
+        if (active.innerHTML.toString().toLowerCase() === product[i].type) {
+            productToShow.push(product[i]);
+        }                     
+    };
+}
+
+
