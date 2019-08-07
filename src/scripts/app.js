75f6d22b0f8a183b597828cb872c62bfdea9b3dd
Patryk Buła <patryk@bulczan.pl>
2019-08-07 21:30:30 +0200

    Resolve conflicts

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,3 @@
-console.log('hello');
+import toggleMainMenu from './header.js';
+
+toggleMainMenu();
--- src/scripts/templatesGenerator.js
+++ src/scripts/templatesGenerator.js
@@ -2,29 +2,28 @@ const Mustache = require('mustache');
 const post = require('./post_data.js');
 console.log(post);
 
-(function loadPost() {
-    const postTemplate = document.getElementById('post-box-column-template').innerHTML;
-    Mustache.parse(postTemplate);
-    let rendered = '';
-    for (let i = 0; i < post.length; i++) {
-        rendered += Mustache.render(postTemplate, post[i]);
-        
-    }
-    document
-        .querySelector('.section--blog-posts .row-posts')
-        .insertAdjacentHTML('beforeend', rendered);
+(function loadPost () {
+  const postTemplate = document.getElementById('post-box-column-template').innerHTML;
+  Mustache.parse(postTemplate);
+  let rendered = '';
+  for (let i = 0; i < post.length; i++) {
+    rendered += Mustache.render(postTemplate, post[i]);
+  }
+  document
+    .querySelector('.section--blog-posts .row-posts')
+    .insertAdjacentHTML('beforeend', rendered);
 })();
 
-function loadProducts(productList) {
-
-    const productTemplate = document.getElementById('product-box-column-template').innerHTML;
-    Mustache.parse(productTemplate); 
-    let rendered = '';
-    for (let i = 0; i < productList.length; i++) {
-        rendered += Mustache.render(productTemplate, productList[i]);
-    }
-    document
-        .querySelector('.section--products .row-products')
-        .insertAdjacentHTML('beforeend', rendered);
+function loadProducts (productList) {
+  const productTemplate = document.getElementById('product-box-column-template')
+    .innerHTML;
+  Mustache.parse(productTemplate);
+  let rendered = '';
+  for (let i = 0; i < productList.length; i++) {
+    rendered += Mustache.render(productTemplate, productList[i]);
+  }
+  document
+    .querySelector('.section--products .row-products')
+    .insertAdjacentHTML('beforeend', rendered);
 }
 module.exports = loadProducts;
