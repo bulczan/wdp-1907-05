22ef71a436ae60465f09d36ca4daa5bfab3f27e7
Michał Nowakowski <nowakowskimichal1988@gmail.com>
2019-08-19 11:34:06 +0200

    WDP-190705-20.3

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,6 @@
 import toggleMainMenu from './header.js';
 import './product_score.js';
-import flktyblog from './latest_blog.js';
+import flickityBlog from './latest_blog.js';
 
 toggleMainMenu();
-flktyblog();
+flickityBlog();
